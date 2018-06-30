a7118ff48267de77a4cb0d17d375dbab8f293025
Quintus Chen <quintus.guo.chen@outlook.com>
2018-06-30 12:12:27 +1200

    Added JUnit Reporter for Jenkins integration.

--- configs/protractor.config.ts
+++ configs/protractor.config.ts
@@ -3,15 +3,18 @@ import {SpecReporter} from "jasmine-spec-reporter";
 import {GLOBAL_SETTINGS} from "../e2e/support/constants";
 import * as protractorConf from "./protractor.config.json";
 import * as htmlReportConf from "./htmlReporter.config.json";
+import * as junitReportConf from "./junitReporter.config.json";
 import * as webDriverConf from "../node_modules/protractor/node_modules/webdriver-manager/selenium/update-config.json";
 
 const HtmlReporter = require('protractor-beautiful-reporter');
+const JasmineReporters = require('jasmine-reporters');
 
 let _config: Config = {
     seleniumServerJar: (<any>webDriverConf).standalone.last,
     chromeDriver: (<any>webDriverConf)['chrome'].last,
     onPrepare: () => {
         jasmine.getEnv().addReporter(new SpecReporter());
+        jasmine.getEnv().addReporter(new JasmineReporters.JUnitXmlReporter(junitReportConf));
         // Add a screen shot reporter:
         jasmine.getEnv().addReporter(new HtmlReporter(htmlReportConf).getJasmine2Reporter());
 
